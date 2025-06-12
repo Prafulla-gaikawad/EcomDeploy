18970d0ff9e75543e465c051f0bba0f6a655f256
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-06-12 18:45:43 +0530

    headers added

--- server/server.js
+++ server/server.js
@@ -43,6 +43,14 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => res.send("Hello world"));
 
